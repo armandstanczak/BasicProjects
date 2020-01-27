114bf0925977f49f584bb1924a212e9051ed96d2
Armand Stanczak <armandstanczak@gmail.com>
2020-01-27 23:14:32 +0100

    Update

--- src/simpletasks/Task1_PrintHello.java
+++ src/simpletasks/Task1_PrintHello.java
@@ -1,16 +1,11 @@
 package simpletasks;
 
-/**
- *
- * @author Armand Stańczak
- */
 public class Task1_PrintHello {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+
+        System.out.println("Hello world\nFirst project");
+        
     }
     
 }
